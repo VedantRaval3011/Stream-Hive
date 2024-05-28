7b9540c1c242ccd8b0394c66ce5b400667304659
VedantRaval3011 <vedantraval30@gmail.com>
2024-05-28 09:54:30 +0530

    comments in app.js

--- src/app.js
+++ src/app.js
@@ -6,11 +6,14 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true  // Allow credentials (cookies, authorization headers, etc.)
 }))
 
+// Use middleware to parse JSON request bodies, limiting the size to 16kb
 app.use(express.json({limit:"16kb"}))
+// Use middleware to parse URL-encoded request bodies, allowing rich objects and arrays, limiting the size to 16kb
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
+// Serve static files from the "public" directory
 app.use(express.static("public"))
 app.use(cookieParser())
 
